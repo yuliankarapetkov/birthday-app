397c4a6b29a545189f96e64c0441276170d6d2df
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-05-22 16:50:28 +0300

    Added a temporary store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+import { Store } from '../store';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -34,7 +35,9 @@ const firebaseConfig = {
         AppRoutingModule,
         SharedModule
     ],
-    providers: [],
+    providers: [
+        Store
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
