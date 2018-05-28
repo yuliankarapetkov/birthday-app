720ec609b9fdbadf70564d1e6174398c62de25d6
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-05-28 15:48:44 +0300

    Added moment.js and created a DateService to calculate age, next birthday and days until

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -4,6 +4,8 @@ import { Store } from '../../store';
 import { takeWhile } from 'rxjs/internal/operators';
 import { Observable } from 'rxjs/index';
 
+import { DateService } from '../shared/services/date/date.service';
+
 @Component({
     selector: 'friends-root',
     templateUrl: './friends.component.html',
@@ -12,24 +14,34 @@ import { Observable } from 'rxjs/index';
 export class FriendsComponent implements OnInit, OnDestroy {
     private componentAlive = true;
 
-    shee: any;
-
     friends$: Observable<Friend[]>;
 
     constructor(
         private store: Store,
-        private friendsService: FriendsService
+        private friendsService: FriendsService,
+        private dateService: DateService
     ) { }
+
+    getNextBirthday(birthdayTimestamp: number) {
+        return this.dateService.getNextBirthday(new Date(birthdayTimestamp));
+    }
+
+    getDaysUntilNextBirthday(birthdayTimestamp: number) {
+        const nextBirthday = this.getNextBirthday(birthdayTimestamp);
+        return this.dateService.getDaysUntil(nextBirthday);
+    }
+
+    getTurningAge(birthdayTimestamp: number): number {
+        const daysUntilNextBirthday = this.getDaysUntilNextBirthday(birthdayTimestamp),
+            age = this.dateService.getAge(new Date(birthdayTimestamp));
+        return daysUntilNextBirthday === 0 ? age : age + 1;
+    }
 
     ngOnInit() {
         this.friends$ = this.store.select<Friend[]>('friends');
         this.friendsService.friends$
             .pipe(takeWhile(() => this.componentAlive))
-            .subscribe(d => {
-                this.shee = d.map(a => ({ ...a, birthday: a.birthday }));
-
-                console.log('shee', this.shee);
-            });
+            .subscribe();
     }
 
     ngOnDestroy() {
