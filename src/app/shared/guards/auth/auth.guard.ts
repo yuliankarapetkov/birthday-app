ae2d82cdeb428e81413e4f037d6d5b291b9e42f1
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-06-07 17:50:48 +0300

    added auth guard + redirectTo app 2

---
+++ src/app/shared/guards/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import * as fromStore from '../../../store';
+
+import { SharedModule } from '../../shared.module';
+
+@Injectable({
+    providedIn: SharedModule
+})
+export class AuthGuard implements CanActivate {
+    constructor(private store: Store<fromStore.State>) { }
+
+    canActivate() {
+        return this.store.select(fromStore.getIsLoggedIn);
+    }
+}
